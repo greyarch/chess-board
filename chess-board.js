4f4b9fef1b11b5e77e7976797de7f2a38206d6ea
greyarch <gantcho@greyarch.net>
2013-03-15 16:31:54 +0100

    board creation added

--- chess-board.js
+++ chess-board.js
@@ -1,33 +1,53 @@
-DEFAULT_BOARD = {game: 1, messages: [{text: "Game on!", isMove: false}], pieces: [
-        ["20", "22", "21", "19", "18", "21", "22", "20"],
-        ["23", "23", "23", "23", "23", "23", "23", "23"],
-        ["", "", "", "", "", "", "", ""],
-        ["", "", "", "", "", "", "", ""],
-        ["", "", "", "", "", "", "", ""],
-        ["", "", "", "", "", "", "", ""],
-        ["17", "17", "17", "17", "17", "17", "17", "17"],
-        ["14", "16", "15", "13", "12", "15", "16", "14"]
-    ]};
+createBoard = function() {
+    return {messages: [{text: "Game on!", isMove: false}], pieces: [
+            ["20", "22", "21", "19", "18", "21", "22", "20"],
+            ["23", "23", "23", "23", "23", "23", "23", "23"],
+            ["", "", "", "", "", "", "", ""],
+            ["", "", "", "", "", "", "", ""],
+            ["", "", "", "", "", "", "", ""],
+            ["", "", "", "", "", "", "", ""],
+            ["17", "17", "17", "17", "17", "17", "17", "17"],
+            ["14", "16", "15", "13", "12", "15", "16", "14"]
+        ]};
+}
+
 Games = new Meteor.Collection("games");
 
 if (Meteor.isClient) {
+    Meteor.Router.add({
+        '/': 'start',
+        '/board/:id': function(id) {
+            console.log("board id is", id);
+            Session.set('boardId', id);
+            return 'board';
+        },
+        '*': 'not_found'
+    });
+
+    Template.start.events = {
+        'click button#new-board': function(e) {
+            var boardId = Games.insert(createBoard());
+            Meteor.Router.to("/board/" + boardId);
+        }
+    };
+
     Template.board.game = function() {
-        return Games.findOne({game: 1});
+        return Games.findOne({_id: Session.get('boardId')});
     };
 
     Template.board.events = {
         'keypress input#message': function(e) {
             if (e.charCode == '13') {
                 var msg = $('input#message').val();
-                var game = Games.findOne({game: 1});
+                var game = Games.findOne({_id: Session.get('boardId')});
                 var messages = game.messages;
                 if (m = msg.match(/^([abcdefgh])([12345678])-([abcdefgh])([12345678])$/i)) {
                     var newSet = move(game.pieces, m);
                     messages.push({text: msg, isMove: true});
-                    Games.update({game: 1}, {$set: {pieces: newSet, messages: messages}});
+                    Games.update({_id: Session.get('boardId')}, {$set: {pieces: newSet, messages: messages}});
                 } else {
                     messages.push({text: msg, isMove: false});
-                    Games.update({game: 1}, {$set: {messages: messages}});
+                    Games.update({_id: Session.get('boardId')}, {$set: {messages: messages}});
                 }
                 $('input#message').val("");
             }
@@ -36,7 +56,7 @@ if (Meteor.isClient) {
 
     Template.navigation.events = {
         'click button#reset': function() {
-            Games.update({game: 1}, {$set: DEFAULT_BOARD});
+            Games.update({_id: Session.get('boardId')}, {$set: createBoard()});
         }
     };
 
@@ -47,12 +67,4 @@ if (Meteor.isClient) {
         pieces[8 - m[4]][m[3].toLowerCase().charCodeAt(0) - 97] = pc;
         return pieces;
     }
-}
-
-if (Meteor.isServer) {
-    Meteor.startup(function() {
-        if (Games.find({}).count() == 0) {
-            Games.insert(DEFAULT_BOARD);
-        }
-    });
 }
