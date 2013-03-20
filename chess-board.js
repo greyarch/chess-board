a46f87dfad085adaf5460a54ecd85fabc5f0ec99
greyarch <gantcho@greyarch.net>
2013-03-20 18:23:19 +0100

    adding/removing a piece on the board

--- chess-board.js
+++ chess-board.js
@@ -1,15 +1,21 @@
+var PIECE_SET = {
+    WP: "17", WR: "14", WN: "16", WB: "15", WQ: "13", WK: "12",
+    BP: "23", BR: "20", BN: "22", BB: "21", BQ: "19", BK: "18",
+    EMPTY: ""
+}
+
 createBoard = function() {
     return {messages: [{text: "Game on!", isMove: false}], pieces: [
-            ["20", "22", "21", "19", "18", "21", "22", "20"],
-            ["23", "23", "23", "23", "23", "23", "23", "23"],
+            [PIECE_SET.BR, PIECE_SET.BN, PIECE_SET.BB, PIECE_SET.BQ, PIECE_SET.BK, PIECE_SET.BB, PIECE_SET.BN, PIECE_SET.BR],
+            [PIECE_SET.BP, PIECE_SET.BP, PIECE_SET.BP, PIECE_SET.BP, PIECE_SET.BP, PIECE_SET.BP, PIECE_SET.BP, PIECE_SET.BP],
             ["", "", "", "", "", "", "", ""],
             ["", "", "", "", "", "", "", ""],
             ["", "", "", "", "", "", "", ""],
             ["", "", "", "", "", "", "", ""],
-            ["17", "17", "17", "17", "17", "17", "17", "17"],
-            ["14", "16", "15", "13", "12", "15", "16", "14"]
+            [PIECE_SET.WP, PIECE_SET.WP, PIECE_SET.WP, PIECE_SET.WP, PIECE_SET.WP, PIECE_SET.WP, PIECE_SET.WP, PIECE_SET.WP],
+            [PIECE_SET.WR, PIECE_SET.WN, PIECE_SET.WB, PIECE_SET.WQ, PIECE_SET.WK, PIECE_SET.WB, PIECE_SET.WN, PIECE_SET.WR]
         ]};
-}
+};
 
 Games = new Meteor.Collection("games");
 if (Meteor.isClient) {
@@ -26,6 +32,7 @@ if (Meteor.isClient) {
     Template.board.game = function() {
         return Games.findOne({_id: Session.get('boardId')});
     };
+
     Template.board.events = {
         'keypress input#message': function(e) {
             if (e.charCode == '13') {
@@ -33,13 +40,15 @@ if (Meteor.isClient) {
                 var game = Games.findOne({_id: Session.get('boardId')});
                 var messages = game.messages;
                 if (m = msg.match(/^([abcdefgh])([12345678])-([abcdefgh])([12345678])$/i)) {
-                    var newSet = move(game.pieces, m);
+                    move(game.pieces, m);
                     messages.push({text: msg, isMove: true});
-                    Games.update({_id: Session.get('boardId')}, {$set: {pieces: newSet, messages: messages}});
+                } else if(m = msg.match(/^([w|b][p|r|n|b|q|k]|empty)@([abcdefgh])([12345678])$/i)) {
+                    setPieceAt(game.pieces, m[2], m[3], PIECE_SET[m[1].toUpperCase()]);
+                    messages.push({text: msg, isMove: true});
                 } else {
                     messages.push({text: msg, isMove: false});
-                    Games.update({_id: Session.get('boardId')}, {$set: {messages: messages}});
                 }
+                    Games.update({_id: Session.get('boardId')}, {$set: {pieces: game.pieces, messages: messages}});
                 $('input#message').val("");
             }
         }
@@ -61,9 +70,17 @@ if (Meteor.isClient) {
 
     move = function(pieces, m) {
         console.log("move", m);
-        var pc = pieces[8 - m[2]][m[1].toLowerCase().charCodeAt(0) - 97];
-        pieces[8 - m[2]][m[1].toLowerCase().charCodeAt(0) - 97] = "";
-        pieces[8 - m[4]][m[3].toLowerCase().charCodeAt(0) - 97] = pc;
+        var pc = getPieceAt(pieces, m[1], m[2]);
+        setPieceAt(pieces, m[1], m[2], "");
+        setPieceAt(pieces, m[3], m[4], pc);
         return pieces;
-    }
+    };
+
+    getPieceAt = function(pieces, col, row) {
+        return pieces[8 - row][col.toLowerCase().charCodeAt(0) - 97];
+    };
+
+    setPieceAt = function(pieces, col, row, pc) {
+        pieces[8 - row][col.toLowerCase().charCodeAt(0) - 97] = pc;
+    };
 }
